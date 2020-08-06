78f982ed955a93f639326cb65a021bc37a9cb1fe
Rabchuk Oleksandr <rabchuk.alexandr@ukr.net>
2020-08-07 00:53:51 +0300

    Routes&Validation

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 
+const  connectDB = require('./config/db')
+
 const PORT = process.env.PORT || 5000;
 
+// Connect DB
+connectDB();
+// Init Middleware
+app.use(express.json({extended:false}));
 
 
 app.get('/',(req,res)=> res.send('API Running'))
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
+
+//Define routes
+
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
